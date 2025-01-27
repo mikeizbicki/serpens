dd48180b51fa4fe93788fca9e70cdfc5b27f4dee
Mike Izbicki <mike@izbicki.me>
2025-01-27 13:23:52 -0800

    train-seed.sh

--- scripts/train-seed.sh
+++ scripts/train-seed.sh
@@ -3,12 +3,13 @@
 set -ex
 commit=$(sh scripts/get_sane_commit_hash.sh)
 
-common="--comment=commit-$commit- --n_env=128 --batch_size=256 --features_dim=256 --gamma=0.9 --policy=ObjectCnn --action_space=zelda-all --task=attack"
+common="--comment=commit-$commit- --n_env=128 --batch_size=32 --features_dim=256 --gamma=0.9 --policy=ObjectCnn --action_space=zelda-all --task=attack --seed=0"
 
-CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --seed=0 --fwat=600 > nohup/nohup.0 &
-CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --seed=1 --fwat=600 > nohup/nohup.1 &
-CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --seed=0 --fwat=900 > nohup/nohup.2 &
-CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --seed=1 --fwat=900 > nohup/nohup.3 &
-CUDA_VISIBLE_DEVICES=4 nohup python3 Mundus/train.py $common --seed=0 > nohup/nohup.4 &
-CUDA_VISIBLE_DEVICES=5 nohup python3 Mundus/train.py $common --seed=1 > nohup/nohup.5 &
+CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --n_env=16 --fwat=600 > nohup/nohup.0 &
+CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --n_env=32 --fwat=600 > nohup/nohup.1 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --n_env=64 --fwat=600 > nohup/nohup.2 &
+CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --n_env=16 --n_steps=64 --fwat=600 > nohup/nohup.0 &
+CUDA_VISIBLE_DEVICES=4 nohup python3 Mundus/train.py $common --n_env=32 --n_steps=64 --fwat=600 > nohup/nohup.1 &
+CUDA_VISIBLE_DEVICES=5 nohup python3 Mundus/train.py $common --n_env=64 --n_steps=64 --fwat=600 > nohup/nohup.2 &
 
+tail -f nohup/nohup.?

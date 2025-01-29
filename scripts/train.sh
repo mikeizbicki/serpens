1f9c15c9107cbed7a0df62da3a6eeec9785f052d
Mike Izbicki <mike@izbicki.me>
2025-01-29 00:49:28 -0800

    train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,9 +5,9 @@ commit=$(sh scripts/get_sane_commit_hash.sh)
 
 common="--comment=$commit --task=attack"
 
-CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --warmstart=1cdbbae7 > nohup/nohup.0 &
-CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --warmstart=a7bf5c48 > nohup/nohup.1 &
-CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --warmstart=f99d1871 > nohup/nohup.2 &
-CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --warmstart=51cb494b > nohup/nohup.3 &
+CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --reset_method="spiders enemy" > nohup/nohup.0 &
+CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --reset_method="octo enemy" > nohup/nohup.1 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --reset_method="map enemy" > nohup/nohup.2 &
+CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --reset_method="spiders octo enemy" > nohup/nohup.3 &
 
 tail -f nohup/nohup.?

142c80522df0aea1a1bb07fa9503f0bd4957d1f2
Mike Izbicki <mike@izbicki.me>
2025-01-30 21:00:02 -0800

    train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -11,14 +11,16 @@ if [ "$pids" != "" ]; then
     # kill will fail on this pid because the process has finished
     # by the time this command is run;
     # adding the || true ensures that the script doesn't terminate
+    sleep 1
 fi
 
 # start the new scripts
 common="--comment=$commit" 
 
 CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --policy=ObjectCnn --task='onmouse*' > nohup/nohup.0 &
-CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse*' > nohup/nohup.1 &
-CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse_random' > nohup/nohup.2 &
-CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse_enemy' > nohup/nohup.3 &
+CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse*' --reset_method='spider enemy' > nohup/nohup.1 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse*' --reset_method='octo spider enemy' > nohup/nohup.2 &
+CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --policy=EventExtractor --task='onmouse*' > nohup/nohup.3 &
 
+sleep 1
 tail -f nohup/nohup.?

3449d47b05e7624a1eb106f5577671f250196162
Mike Izbicki <mike@izbicki.me>
2025-02-03 14:07:27 -0800

    train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -19,8 +19,8 @@ common="--comment=$commit"
 
 CUDA_VISIBLE_DEVICES=0 nohup python3 Mundus/train.py $common --warmstart=46ca002c --policy=EventExtractor > nohup/nohup.0 &
 CUDA_VISIBLE_DEVICES=1 nohup python3 Mundus/train.py $common --warmstart=f5fb2371 --policy=ContinuousEventExtractor > nohup/nohup.1 &
-CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --warmstart=46ca002c --policy=EventExtractor > nohup/nohup.2 &
-CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --warmstart=f5fb2371 --policy=ContinuousEventExtractor > nohup/nohup.3 &
+CUDA_VISIBLE_DEVICES=2 nohup python3 Mundus/train.py $common --policy=EventExtractor > nohup/nohup.2 &
+CUDA_VISIBLE_DEVICES=3 nohup python3 Mundus/train.py $common --policy=ContinuousEventExtractor > nohup/nohup.3 &
 
 sleep 1
 tail -f nohup/nohup.?
